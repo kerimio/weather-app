9f12f9c1c1c8cad393ef334dff1a560e1c396c47
kerimio <bjkerim@web.de>
2022-02-11 01:09:56 +0100

    Weather API and Background pictures added

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+const api = {
+  key: "a4960f8300da807f576fc9b40fc8fad7",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
